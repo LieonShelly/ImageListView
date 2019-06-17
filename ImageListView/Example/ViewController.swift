fc6c4fb59f0d13b67c3579db71021c01b6c4965b
lieon <lieoncx@gmail.com>
2019-06-17 18:17:38 +0800

    add share framewrk

--- ImageListView/Example/ViewController.swift
+++ ImageListView/Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ImageListView
 
 class ViewController: UIViewController {
 
---
+++ ImageListView/ImageListView.h
@@ -0,0 +1,19 @@
+//
+//  ImageListView.h
+//  ImageListView
+//
+//  Created by lieon on 2019/6/17.
+//  Copyright © 2019 lieon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageListView.
+FOUNDATION_EXPORT double ImageListViewVersionNumber;
+
+//! Project version string for ImageListView.
+FOUNDATION_EXPORT const unsigned char ImageListViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageListView/PublicHeader.h>
+
+
